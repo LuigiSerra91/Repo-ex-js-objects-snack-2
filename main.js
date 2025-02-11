781875dfa1d170cfd13c19051664095654562511
LuigiSerra91 <luigi_serra91@hotmail.it>
2025-02-11 16:20:47 +0100

    add question 3

--- main.js
+++ main.js
@@ -28,3 +28,23 @@ console.log(secondBurger.ingredients[0]); // ? */
 
 // viene stampato Salad in tutti e 2
 
+// Code Question 3
+
+/* const hamburger = {
+    name: "Cheese Burger",
+    weight: 250,
+    maker: {
+        name: "Anonymous Chef",
+        restaurant: {
+            name: "Hyur's Burgers",
+            address: "Main Street, 123",
+            isOpen: true,
+        },
+        age: 29
+    }
+};
+
+const secondBurger = structuredClone(hamburger);
+const thirdBurger = structuredClone(hamburger); */
+
+// sono stati creati 9 oggetti 
