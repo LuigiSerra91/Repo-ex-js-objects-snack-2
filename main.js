7c36c550771a3a8bb8704d594855d36d6ba79657
LuigiSerra91 <luigi_serra91@hotmail.it>
2025-02-11 16:13:22 +0100

    add question 1

---
+++ main.js
@@ -0,0 +1,13 @@
+//Code Question 1
+
+/* const hamburger = { name: "Cheese Burger", weight: 250 };
+const secondBurger = hamburger;
+secondBurger.name = 'Double Cheese Burger';
+secondBurger.weight = 500;
+
+console.log(hamburger.name); // ?
+console.log(secondBurger.name ); // ? */
+
+// viene stampato Double cheese burger
+
+
