e85ce3a94496e0cbb88ca60a388a350c63ac9a87
LuigiSerra91 <luigi_serra91@hotmail.it>
2025-02-11 16:15:29 +0100

    add questio 2

--- main.js
+++ main.js
@@ -8,6 +8,23 @@ secondBurger.weight = 500;
 console.log(hamburger.name); // ?
 console.log(secondBurger.name ); // ? */
 
-// viene stampato Double cheese burger
+// viene stampato Double cheese burger in tutti e 2
 
+
+//Code Question 2
+
+
+/* const hamburger = {
+    name: "Cheese Burger",
+    weight: 250,
+    ingredients: ["Cheese", "Meat", "Bread", "Tomato"]
+};
+
+const secondBurger = {...hamburger};
+secondBurger.ingredients[0] = "Salad";
+
+console.log(hamburger.ingredients[0]); // ?
+console.log(secondBurger.ingredients[0]); // ? */
+
+// viene stampato Salad in tutti e 2
 
